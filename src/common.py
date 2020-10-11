f9a5ad8b6852ba3c72e969e1360c6431cb01246f
manojk-at-739803869088 <manojk@bizcloudexperts.com>
2020-10-11 17:17:29 -0500

    Added CRM API services code

---
+++ src/common.py
@@ -0,0 +1,99 @@
+import os
+import json
+import boto3
+import logging
+import psycopg2
+from datetime import datetime,timezone
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def modify_date(x):
+    try:
+        if x == None:
+            return 'null'
+        else:
+            return x.isoformat()
+    except Exception as e:
+        logging.exception("DateConversionError: {}".format(e))
+
+def update_date(load_date):
+    try:
+        if load_date == None:
+            return '9999-12-31 00:00:00'
+        else:
+            return load_date.strftime("%m/%d/%Y, %H:%M:%S")
+    except Exception as e:
+        logging.exception("UpdateDateError: {}".format(e))
+
+def get_timestamp(param_name):
+    try:
+        client = boto3.client('ssm')
+    except Exception as e:
+        logging.exception("SsmInitializationError: {}".format(e))
+        raise InitializationError(json.dumps({"httpStatus": 400, "message": "ssm initialization error."}))
+
+    try:
+        response = client.get_parameter(
+            Name=param_name
+        )
+        return response["Parameter"]["Value"]
+    except Exception as e:
+        logging.exception("SsmGetParameterError: {}".format(e))
+        raise GetParameterError(json.dumps({"httpStatus": 400, "message": "ssm get parameter error."}))
+
+def set_timestamp(param_name, param_value):
+    try:
+        client = boto3.client('ssm')
+    except Exception as e:
+        logging.exception("SsmInitializationError: {}".format(e))
+        raise InitializationError(json.dumps({"httpStatus": 400, "message": "ssm initialization error."}))
+
+    try:
+        client.put_parameter(
+            Name=param_name,
+            Value=param_value,
+            Type='String',
+            Overwrite=True
+        )
+    except Exception as e:
+        logging.exception("SsmSetParameterError: {}".format(e))
+        raise SetParameterError(json.dumps({"httpStatus": 400, "message": "ssm set parameter error."}))
+
+def execute_db_query(query):
+    try:
+        con=psycopg2.connect(dbname = os.environ['db_name'], host=os.environ['db_host'],
+                            port= os.environ['db_port'], user = os.environ['db_username'], password = os.environ['db_password'])
+        con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = con.cursor()
+        cur.execute(query)
+        con.commit()
+        results = cur.fetchall()
+        cur.close()
+        con.close()
+        return results
+    except Exception as e:
+        logging.exception("DatabaseError: {}".format(e))
+        raise DatabaseError(json.dumps({"httpStatus": 400, "message": "Database error."}))
+
+def s3GetObject(bucket,key):
+    
+    try:
+        s3 = boto3.resource('s3')
+    except Exception as e:
+        logging.exception("S3InitializationError: {}".format(e))
+        raise InitializationError(json.dumps({"httpStatus": 400, "message": "s3 initialization error."}))
+
+    try:
+        data = s3.Object(bucket, key).get()['Body'].read()
+        return data
+
+    except Exception as e:
+        logging.exception("GetS3ObjectError: {}".format(e))
+        raise GetS3ObjectError(json.dumps({"httpStatus": 400, "message": "S3 Get object error."}))
+        
+class InitializationError(Exception): pass
+class GetParameterError(Exception): pass
+class SetParameterError(Exception): pass
+class DatabaseError(Exception): pass
+class GetS3ObjectError(Exception): pass
---
+++ src/global-customers.py
@@ -0,0 +1,114 @@
+import os
+import json
+import logging
+import requests
+import pytz
+import boto3
+from datetime import datetime,timezone
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+from src.common import modify_date
+from src.common import execute_db_query
+from src.common import get_timestamp
+from src.common import set_timestamp
+from src.common import s3GetObject
+
+headers = {'content-type': 'application/json'}
+tz = pytz.timezone('US/Central')
+fmt = '%Y-%m-%d %H:%M:%S'
+s3 = boto3.resource('s3')
+
+bucket = os.environ['s3_bucket']
+key = os.environ['s3_key']
+
+def handler(event, context):
+    logger.info("Event: {}".format(json.dumps(event)))
+    try:
+        url = os.environ['globalcustomer_table_url']
+        timestamp_param_name = os.environ['timestamp_parameter']
+    except Exception as e:
+        logging.exception("EnvironmentVariableError: {}".format(e))
+        raise EnvironmentVariableError(json.dumps({"httpStatus": 400, "message": "Environment variable not set."}))
+
+    if "start_from" in event:
+        start_record = event["start_from"]
+        s3_data = s3GetObject(bucket,key)
+        records = eval(s3_data)
+        print("Records are:", records)
+    else:
+        start_record = 0
+        records = initial_execution(timestamp_param_name)
+        # event.Records = records
+
+    stop_record = (start_record + 100) if (start_record + 100) < len(records) else len(records)
+
+    logger.info("Executing from array index {} to {}".format(start_record, stop_record))
+    count = 0
+    for record in records[start_record:stop_record]:
+        count=count+1
+        if count % 100 == 0:
+            logger.info("Working on processing element number: {}".format(records.index(record)))
+
+        data = json.dumps(convert_records(record))
+        try:
+            r = requests.post(url, headers=headers,data=data)
+            # logger.info("POST API RESPONSE: {}".format(r))
+        except Exception as e:
+            logging.exception("ApiPostError: {}".format(e))
+            set_timestamp(timestamp_param_name, datetime.now(tz).strftime(fmt)) #changed the timestamp
+            raise ApiPostError(json.dumps({"httpStatus": 400, "message": "Api post error."}))
+    
+    logger.info("Execution complete!")
+    event["start_from"] = stop_record
+    if(stop_record != len(records)):
+        logger.info("In progress")
+        event["status"] = "InProgress"
+    else:
+        logger.info("completed!")
+        event["status"] = "Completed"
+        set_timestamp(timestamp_param_name, datetime.now(tz).strftime(fmt))
+    return event
+
+def initial_execution(param_name):
+    time = get_timestamp(param_name)
+    query = 'SELECT new_global_name,bill_to,bill_to_name, care_of_filter, care_of_name, company, customer_type, source_system,subsidiary_consolidation FROM public.global_cust_name WHERE (load_create_date >= \''+time+'\' OR load_update_date >= \''+time+'\')'
+    queryData = execute_db_query(query)
+
+    try:
+        with open('/tmp/global_customers.txt', 'w', encoding='utf-8') as f:
+            f.write(str(queryData))
+        s3.meta.client.upload_file('/tmp/global_customers.txt', bucket, key)
+    except Exception as e:
+        logging.exception("UpdateFileError: {}".format(e))
+        raise UpdateFileError(json.dumps({"httpStatus": 400, "message": "Update file error."}))
+
+    return execute_db_query(query)
+
+def convert_records(data):
+    try:
+        record = {}
+        record["new_global_name"] = data[0]
+        record["bill_to"] = data[1]
+        record["bill_to_name"] = data[2]
+        record["care_of_filter"] = data[3]
+        record["care_of_name"] = data[4]
+        record["company"] = data[5]
+        record["customer_type"] = data[6]
+        record["global_name_match"] = str(data[7])+"-"+str(data[1])
+        record["source_system"] = data[7]
+        record["station"] = "--"
+        record["subsidiary_consolidation"] = data[8]
+        return record
+    except Exception as e:
+        logging.exception("RecordConversionError: {}".format(e))
+        raise RecordConversionError(json.dumps({"httpStatus": 400, "message": "Record conversion error."}))
+
+class EnvironmentVariableError(Exception): pass
+class GetS3ObjectError(Exception): pass
+class InitializationError(Exception): pass
+class UpdateFileError(Exception): pass
+class GetParameterError(Exception): pass
+class RecordConversionError(Exception): pass
+class ApiPostError(Exception): pass
